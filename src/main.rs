ed353e29a93330a79aece12bcd03303c8c68f8ea
Subhayan <subhayan.aws@gmail.com>
2025-01-05 23:12:16 +0100

    simple algo to find the sum of all multiples of 3 or 5 below a given number

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
-// Simple rust code to calculate the nth fibonacci number 
-// Questions:
-// How to make sure that rust accepts a number from the terminal which is u32
+// the sum of all multiples of 3 or 5 below a given number 
 
 
 use std::io;
 
-fn nth_fibonacci_number(value: u64) -> u64 {
-    if value <= 1 { value } else {
-        let mut first: u64 = 0;
-        let mut second: u64 = 1;
-        let mut fibonacci: u64 = 0;
-        for _number in 2..value {
-            fibonacci = first + second;
-            first = second;
-            second = fibonacci;
-        }
-        return fibonacci
+fn is_a_multiple_of_three_or_five(number: u32) -> bool {
+    let remainder_3 = number % 3;
+    let remainder_5 = number % 5;
+    if remainder_3 == 0 {
+        true
+    } else if remainder_5 == 0 {
+        true
+    } else {
+        false
     }
 }
 
 fn main() {
-    println!("Please input the number for which the the nth Fibonacci is desired");
+    println!("Please input the under which multiples of 3 or 5 needs to be added!");
     loop {
         let mut number = String::new();
         io::stdin()
@@ -38,8 +34,13 @@ fn main() {
             println!("This does not make sense provide a number greater than 0.");
             continue;
         } else {
-            let value: u64 = number as u64; // explicit conversion between types
-            let answer: u64 = nth_fibonacci_number(value);
+            let value: u32 = number as u32; // explicit conversion between types
+            let mut answer: u32 = 0;
+            for num in 1..value {
+                if is_a_multiple_of_three_or_five(num) {
+                    answer = answer + num;
+                }
+            }
             println!("The answer is {answer}");
         };
 
