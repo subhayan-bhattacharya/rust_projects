9192eb9f726f1005c55b4700f24be023315e5154
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-01-17 17:06:36 +0100

    playing around with mutable struct references

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ struct Rectangle {
     width: u32
 }
 
-fn calculate_area(dimensions: &Rectangle) -> u32 {
+fn calculate_area(dimensions: &mut Rectangle) -> u32 {
+    dimensions.length = dimensions.length * 5;
     dimensions.length * dimensions.width
 }
 
 fn main() {
     println!("practising rust struct !");
-    let dimensions = Rectangle{
+    let mut dimensions = Rectangle{
         length: 30,
         width: 50
     };
-    let area = calculate_area(&dimensions);
+    let area = calculate_area(&mut dimensions);
+    let new_length = dimensions.length;
     println!("The area is {area}");
+    println!("Accessing original struct field {new_length}");
 }
