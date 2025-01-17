ee30a1007eee8766f25f8f18c2057630ac1f993e
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-01-17 16:31:21 +0100

    area function using tuples

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 // building structs in Rust
-fn calculate_area(length: u32, width: u32) -> u32 {
-    length * width
+fn calculate_area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
 
 fn main() {
     println!("practising rust struct !");
-    let length = 30;
-    let width = 50;
-    let area = calculate_area(length, width);
+    let dimensions = (50, 30);
+    let area = calculate_area(dimensions);
     println!("The area is {area}");
 }
